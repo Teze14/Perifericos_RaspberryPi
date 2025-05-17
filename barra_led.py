3cc946c1a8954bfc7d292a4559ae47e243160813
Jesús Emiliano García Jiménez <143450321+Teze14@users.noreply.github.com>
2025-05-16 18:56:48 -0600

    Update barra_led.py

--- barra_led.py
+++ barra_led.py
@@ -14,18 +14,4 @@ try:
             sleep(0.5)
 except KeyboardInterrupt:
     graph.off()
-    print("apagado")"
-
-encooder rotacional:
-"from gpiozero import RotaryEncoder
-from signal import pause
-
-encoder = RotaryEncoder(a=5, b=6, max_steps=100)
-
-def posicion():
-    print(f"Posición actual: {encoder.steps}")
-
-encoder.when_rotated = posicion
-
-print("gira la perilla")
-pause()
+    print("apagado")
